1608431ca5f25e2d956e388b237bc6e24689b84b
Riku Hara <yuanliqu@gmail.com>
2025-04-02 13:15:08 +0900

    Update ContentView.swift
    contentViewが初期のままでした

--- GroceryList/ContentView.swift
+++ GroceryList/ContentView.swift
@@ -6,19 +6,121 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+    @State private var item: String = ""
+    @FocusState private var isFocused: Bool
+    
+    func addEssentialFoods() {
+        modelContext.insert(Item(title: "Milk", isCompleted: false))
+        modelContext.insert(Item(title: "Pasta & Rice", isCompleted: true))
+        modelContext.insert(Item(title: "Chease & Eggs", isCompleted: .random()))
+        modelContext.insert(Item(title: "Udon Noodles", isCompleted: .random()))
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(items) { item in
+                    Text(item.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
+                        .foregroundStyle(item.isCompleted == false ? Color.primary : Color.accentColor)
+                        .strikethrough(item.isCompleted)
+                        .italic(item.isCompleted)
+                        .swipeActions {
+                            Button(role: .destructive) {
+                                withAnimation{
+                                    modelContext.delete(item)
+                                }
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
+                        .swipeActions(edge: .leading) {
+                            Button("Done",systemImage: item.isCompleted == false ? "checkmark.circle" : "x.circle") {
+                                item.isCompleted.toggle()
+                            }
+                            .tint(item.isCompleted == false ? .green : .accentColor)
+                        }
+                }
+            }
+            .navigationTitle("Grocery List")
+            .toolbar {
+                if items.isEmpty {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            addEssentialFoods()
+                        } label: {
+                            Label("Essentials", systemImage: "carrot")
+                        }
+                    }
+                }
+            }
+            .overlay {
+                if items.isEmpty {
+                    ContentUnavailableView("Empty Cart", systemImage: "cart.circle",
+                        description: Text("Add some items to the shopping List"))
+                }
+            }
+            .safeAreaInset(edge: .bottom) {
+                VStack(spacing: 12) {
+                    TextField("", text: $item)
+                        .textFieldStyle(.plain)
+                        .padding(12)
+                        .background(.tertiary)
+                        .cornerRadius(12)
+                        .font(.title.weight(.light))
+                        .focused($isFocused)
+                    
+                    Button {
+                        guard !item.isEmpty else {
+                            return
+                        }
+                        
+                        let newItem = Item(title: item, isCompleted: false)
+                        modelContext.insert(newItem)
+                        item = ""
+                        isFocused = false
+                    } label: {
+                        Text("Save")
+                            .font(.title2.weight(.medium))
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle)
+                    .controlSize(.extraLarge)
+                }
+                .padding()
+                .background(.bar)
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
+#Preview("Sample Data") {
+    let sampleData: [Item] = [
+        Item(title: "カップ麺", isCompleted: false),
+        Item(title: "にんじん", isCompleted: false),
+        Item(title: "豆腐", isCompleted: .random()),
+        Item(title: "Milk", isCompleted: .random())
+    ]
+    
+    let container = try! ModelContainer(for: Item.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    for item in sampleData {
+        container.mainContext.insert(item)
+    }
+    
+    return ContentView()
+        .modelContainer(container)
+    
+}
+
+#Preview("Empty List") {
     ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
 }
